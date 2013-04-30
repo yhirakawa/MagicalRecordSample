4dadade6d66233ef33c7914f0691701ed9d559b4
Yuta Hirakawa <yhirakawa@sonix.asia>
2013-04-30 22:52:43 +0900

    Initial Commit

---
+++ MagicalRecordSample/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  MagicalRecordSample
+//
+//  Created by yuta hirakawa on 2013/04/30.
+//  Copyright (c) 2013年 yuta hirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ MagicalRecordSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MagicalRecordSample
+//
+//  Created by yuta hirakawa on 2013/04/30.
+//  Copyright (c) 2013年 yuta hirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
